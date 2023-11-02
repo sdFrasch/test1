0a044e917ec92538f2481482c12eeeeee9a618cf
sdFrasch <frasch.sam@gmail.com>
2023-11-01 23:31:02 -0400

    lab06

--- src/Navbar1.js
+++ src/Navbar1.js
@@ -17,9 +17,6 @@ function Navbar() {
               <a className="nav-link" href="#">About</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Services</a>
-            </li>
-            <li className="nav-item">
               <a className="nav-link" href="#">Contact</a>
             </li>
           </ul>
