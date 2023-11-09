62b2904eb6782fec5870a4d1d4b0654b3b0faafb
sdFrasch <frasch.sam@gmail.com>
2023-11-09 16:17:27 -0500

    lab actually complete hopefully

--- src/Navbar1.js
+++ src/Navbar1.js
@@ -3,26 +3,26 @@ import "bootstrap/dist/css/bootstrap.css";
 
 function Navbar() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <Link className="navbar-brand text-white" to="/">NavBar</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <span className="navbar-toggler-icon"></span>
+</button>
+<div className="collapse navbar-collapse" id="navbarNav">
+  <ul className="navbar-nav">
+    <li className="nav-item active">
+      <Link className="nav-link text-white" to="/">Home <span className="sr-only"></span></Link>
+    </li>
+          <li className="nav-item">
+              <Link className="nav-link text-white" to="/Card">Card</Link>
+    </li> 
+          <li className="nav-item">
+               <Link className="nav-link text-white" to="/Contact">Contact</Link>
+          </li>
+       </ul>
+    </div>
+  </nav>
+);
   }
   
   export default Navbar;
