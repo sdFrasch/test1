e1af68c5c5202e6cc386dbe99fcf961c1e808991
sdFrasch <frasch.sam@gmail.com>
2023-11-09 17:32:39 -0500

    lab 7 final rev

--- src/Contact.js
+++ src/Contact.js
@@ -7,28 +7,28 @@ function Contact() {
       <form>
         <div style={inputContainerStyle}>
             <div className="form-group">
-                <label htmlFor="firstname">First Name</label>
+                <label htmlFor="firstname"></label>
                 <input type="text" className="form-control" style= {inputStyle} id="firstname" placeholder="Enter your first name" />
              </div>
             <div className="form-group">
-                <label htmlFor="lastname">Last Name</label>
+                <label htmlFor="lastname"></label>
                 <input type="text" className="form-control" style= {inputStyle} id="lastname" placeholder="Enter your last name" />
             </div>
             <div className="form-group">
-                <label htmlFor="number">Phone Number</label>
+                <label htmlFor="number"></label>
                 <input type="text" className="form-control" style= {inputStyle} id="phonenumber" placeholder="Enter your phone number" />
             </div>
             <div className="form-group">
-                <label htmlFor="email">Email address</label>
+                <label htmlFor="email"></label>
                 <input type="email" className="form-control" style= {inputStyle} id="email" placeholder="Enter your email address" />
             </div>
             <div className="form-group">
-                <label htmlFor="country">Country</label>
+                <label htmlFor="country"></label>
                 <input type="text" className="form-control" style= {inputStyle} id="country" placeholder="Enter your country" />
             </div>
         
             <div className="form-group">
-                <label htmlFor="message">Message</label>
+                <label htmlFor="message"></label>
                 <textarea className="form-control" style= {inputStyle} id="message" rows="5" placeholder="Your Message"></textarea>
             
             </div>        
